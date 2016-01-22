2966fb061236496e35fd5938bd499f71c4d3cd67
Timvangool <timvangool@hotmail.com>
2016-01-22 17:11:11 +0100

    22-01-2016
    Added player class W.I.P.

---
+++ TextBasedAdventure/TextBasedAdventure/Player.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextBasedAdventure
+{
+    class Player
+    {
+        private string playerName;
+        private int playerLives, playerLevel, playerMoney;
+
+        public Player()
+        {
+
+        }
+
+        public Player(string playerName, int playerLives, int playerLevel)
+        {
+            
+        }
+
+        #region Get Set
+        public string GetPlayerName()
+        {
+            return this.playerName;
+        }
+
+        public void SetPlayerName(string playerName)
+        {
+            this.playerName = playerName;
+        }
+
+        public int GetPlayerLevel()
+        {
+            return playerLevel;
+        }
+
+        public void SetPlayerLevel(int playerLevel)
+        {
+            this.playerLevel = playerLevel;
+        }
+
+        public int GetPlayerLives()
+        {
+            return playerLives;
+        }
+
+        public void SetPlayerLives(int playerLives)
+        {
+            this.playerLives = playerLives;
+        }
+
+        public int GetPlayerMoney()
+        {
+            return playerMoney;
+        }
+
+        public void SetPlayerMoney(int playerMoney)
+        {
+            this.playerMoney = playerMoney;
+        }
+        #endregion
+    }
+}
